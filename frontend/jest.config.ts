9917108ef3729f2d7fc71bb20dbc9eaeba4a2d2a
EstebanBedoya <estebegi@gmail.com>
2026-03-24 02:41:43 -0500

    feat(frontend): scaffold React 19 + Vite + Redux Toolkit

---
+++ frontend/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  transform: { '^.+\\.(t|j)sx?$': ['ts-jest', { tsconfig: { jsx: 'react-jsx' } }] },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: { 
+    '\\.(css|less|scss)$': 'identity-obj-proxy', 
+    '\\.(jpg|png|svg|webp)$': '<rootDir>/__mocks__/fileMock.ts' 
+  },
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/main.tsx', '!src/**/*.d.ts'],
+  coverageThreshold: { global: { lines: 80, branches: 80, functions: 80, statements: 80 } },
+}
+
+export default config
